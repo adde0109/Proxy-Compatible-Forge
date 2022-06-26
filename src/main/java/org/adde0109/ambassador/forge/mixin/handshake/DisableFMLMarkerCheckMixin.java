186f46bedb74d4e3c02df90de738811be7ea06ca
Adrian Bergqvist <adde0109@gmail.com>
2022-06-27 01:34:47 +0200

    Fix for ArcLight

--- src/main/java/org/adde0109/ambassador/forge/mixin/handshake/DisableFMLMarkerCheckMixin.java
+++ src/main/java/org/adde0109/ambassador/forge/mixin/handshake/DisableFMLMarkerCheckMixin.java
@@ -1,22 +1,21 @@
 package org.adde0109.ambassador.forge.mixin.handshake;
 
 import net.minecraft.network.handshake.client.CHandshakePacket;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(CHandshakePacket.class)
+@Mixin(value = CHandshakePacket.class, priority = 1200)
 public class DisableFMLMarkerCheckMixin {
 
 
-  @Shadow
-  private String fmlVersion = net.minecraftforge.fml.network.FMLNetworkConstants.NETVERSION;
-
-  @Inject(method = "read", at = @At("RETURN"), cancellable = true)
-  private void onGetFMLVersion(CallbackInfo ci) {
-    this.fmlVersion = net.minecraftforge.fml.network.FMLNetworkConstants.NETVERSION;
+  @Inject(method = "getFMLVersion", at = @At(value = "HEAD"), remap = false, cancellable = true)
+  private void onGetFMLVersion(CallbackInfoReturnable<String> cir) {
+    cir.setReturnValue(FMLNetworkConstants.NETVERSION);
   }
 
 }
