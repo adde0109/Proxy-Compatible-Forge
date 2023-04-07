443ffd295b6fdd233d6efc8a372b915d0b5e75f2
Adrian Bergqvist <adde0109@gmail.com>
2023-04-07 13:23:27 +0200

    Fix null identifier

--- src/main/java/org/adde0109/pcf/mixin/command/WrappableArgumentNodeStubMixin.java
+++ src/main/java/org/adde0109/pcf/mixin/command/WrappableArgumentNodeStubMixin.java
@@ -70,7 +70,7 @@ public class WrappableArgumentNodeStubMixin implements IMixinNodeStub {
   private static <A extends ArgumentType<?>, T extends ArgumentTypeInfo.Template<A>> void wrapInVelocityModArgument(FriendlyByteBuf buf, ArgumentTypeInfo<A, T> serializer, ArgumentTypeInfo.Template<A> properties) {
     ResourceLocation identifier = BuiltInRegistries.COMMAND_ARGUMENT_TYPE.getKey(properties.type());
 
-    if (Initializer.integratedArgumentTypes.contains(identifier.toString())) {
+    if (identifier != null && Initializer.integratedArgumentTypes.contains(identifier.toString())) {
       buf.writeVarInt(BuiltInRegistries.COMMAND_ARGUMENT_TYPE.getId(serializer));
       serializer.serializeToNetwork((T)properties, buf);
       return;
