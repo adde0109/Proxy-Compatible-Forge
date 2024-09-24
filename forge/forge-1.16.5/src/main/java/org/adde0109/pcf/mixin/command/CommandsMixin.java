70a9750695cdd2c9e99386249d3316ec401d430d
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-09-23 18:43:02 -0600

    Use NetworkHooks.getConnectionData(conn) to simplify CommandsMixin slightly

--- forge/forge-1.16.5/src/main/java/org/adde0109/pcf/mixin/command/CommandsMixin.java
+++ forge/forge-1.16.5/src/main/java/org/adde0109/pcf/mixin/command/CommandsMixin.java
@@ -1,13 +1,13 @@
 package org.adde0109.pcf.mixin.command;
 
 import io.netty.buffer.Unpooled;
-import io.netty.util.AttributeKey;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundCustomPayloadPacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraftforge.fml.network.FMLConnectionData;
+import net.minecraftforge.fml.network.NetworkHooks;
 import org.adde0109.pcf.command.IMixinWrappableCommandPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class CommandsMixin {
   @Redirect(method = "sendCommands(Lnet/minecraft/server/level/ServerPlayer;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;send(Lnet/minecraft/network/protocol/Packet;)V"))
   private void sendCommands$grabPacket(ServerGamePacketListenerImpl connection, Packet<?> packet) {
-    FMLConnectionData connectionData = (FMLConnectionData) connection.connection.channel().attr(AttributeKey.valueOf("fml:conndata")).get();
+    FMLConnectionData connectionData = NetworkHooks.getConnectionData(connection.connection);
     if (connectionData != null && connectionData.getChannels().keySet().stream().anyMatch((v) -> v.equals(new ResourceLocation("ambassador:commands")))) {
       FriendlyByteBuf byteBuf = new FriendlyByteBuf(Unpooled.buffer());
       ((IMixinWrappableCommandPacket) packet).write(byteBuf, true);
