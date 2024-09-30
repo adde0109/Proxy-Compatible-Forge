033f89b8f686fd7dae4ee36cd1279dd043cdf9ff
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-09-30 16:51:51 -0600

    Got rid of a non-issue mixin warning and added the licence file

--- forge/forge-1.14.4/src/main/java/org/adde0109/pcf/mixin/v1_14_4/forge/login/ServerLoginPacketListenerPlaceholderMixin.java
+++ forge/forge-1.14.4/src/main/java/org/adde0109/pcf/mixin/v1_14_4/forge/login/ServerLoginPacketListenerPlaceholderMixin.java
@@ -1,15 +1,17 @@
-package org.adde0109.pcf.mixin.common;
+package org.adde0109.pcf.mixin.v1_14_4.forge.login;
 
 import dev.neuralnexus.conditionalmixins.annotations.ReqMCVersion;
 import dev.neuralnexus.taterapi.MinecraftVersion;
+
+import net.minecraft.server.network.ServerLoginPacketListenerImpl;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 @SuppressWarnings({"unused", "RedundantThrows"})
 @ReqMCVersion(min = MinecraftVersion.V1_14, max = MinecraftVersion.V1_16_5)
-@Mixin(targets = "net.minecraft.network.login.ServerLoginNetHandler", priority = 250)
-public class ServerLoginNetHandlerPlaceholderMixin {
+@Mixin(value = ServerLoginPacketListenerImpl.class, priority = 250)
+public class ServerLoginPacketListenerPlaceholderMixin {
     @Unique
     void arclight$preLogin() throws Exception {}
 }
--- forge/forge-1.17.1/src/main/java/org/adde0109/pcf/mixin/v1_17_1/forge/login/ServerLoginPacketListenerPlaceholderMixin.java
+++ forge/forge-1.17.1/src/main/java/org/adde0109/pcf/mixin/v1_17_1/forge/login/ServerLoginPacketListenerPlaceholderMixin.java
@@ -1,16 +1,16 @@
-package org.adde0109.pcf.mixin.common;
+package org.adde0109.pcf.mixin.v1_17_1.forge.login;
 
 import dev.neuralnexus.conditionalmixins.annotations.ReqMCVersion;
 import dev.neuralnexus.taterapi.MinecraftVersion;
+
+import net.minecraft.server.network.ServerLoginPacketListenerImpl;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 @SuppressWarnings({"unused", "RedundantThrows"})
 @ReqMCVersion(min = MinecraftVersion.V1_17)
-@Mixin(
-        targets = {"net.minecraft.server.network.ServerLoginPacketListenerImpl"},
-        priority = 250)
+@Mixin(value = ServerLoginPacketListenerImpl.class, priority = 250)
 public class ServerLoginPacketListenerPlaceholderMixin {
     @Unique
     void arclight$preLogin() throws Exception {}
