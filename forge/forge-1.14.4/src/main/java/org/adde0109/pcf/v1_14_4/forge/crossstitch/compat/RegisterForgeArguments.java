badda62a8cb6caa40e7c080c4d621fb98b1e285e
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-10-18 16:03:31 -0600

    Cleaned up Forge arg 1.14-1.15 fix

--- forge/forge-1.14.4/src/main/java/org/adde0109/pcf/v1_14_4/forge/crossstitch/compat/RegisterForgeArguments.java
+++ forge/forge-1.14.4/src/main/java/org/adde0109/pcf/v1_14_4/forge/crossstitch/compat/RegisterForgeArguments.java
@@ -13,31 +13,10 @@ import net.minecraftforge.server.command.ModIdArgument;
 
 import org.adde0109.pcf.PCF;
 import org.adde0109.pcf.v1_14_4.forge.crossstitch.ArgumentTypesUtil;
-import org.adde0109.pcf.v1_14_4.forge.crossstitch.DummyEnumArgumentSupplier;
 
 public final class RegisterForgeArguments {
     private static final ResourceLocation FORGE_ENUM = new ResourceLocation("forge:enum");
     private static final ResourceLocation FORGE_MODID = new ResourceLocation("forge:modid");
-
-    private RegisterForgeArguments() {}
-
-    public static void applyFixes(ArgumentType<?> argumentType) {
-        if (IS_FORGE_14_15) {
-            if (argumentType.getClass() == EnumArgument.class) {
-                if (!FORGE_MODID_ARG_REGISTERED
-                        && ArgumentTypesUtil.getByClassMap().containsKey(ModIdArgument.class)) {
-                    FORGE_MODID_ARG_REGISTERED = true;
-                }
-                registerForgeEnumArgument();
-            } else if (argumentType.getClass() == ModIdArgument.class) {
-                if (!FORGE_ENUM_ARG_REGISTERED
-                        && ArgumentTypesUtil.getByClassMap().containsKey(EnumArgument.class)) {
-                    FORGE_ENUM_ARG_REGISTERED = true;
-                }
-                registerForgeModIdArgument();
-            }
-        }
-    }
 
     private static final boolean IS_FORGE_14_15 =
             MetaAPI.instance().platform().isForge()
@@ -47,25 +26,37 @@ public final class RegisterForgeArguments {
     private static boolean FORGE_ENUM_ARG_REGISTERED = false;
     private static boolean FORGE_MODID_ARG_REGISTERED = false;
 
-    private static void registerForgeModIdArgument() {
-        if (!FORGE_MODID_ARG_REGISTERED) {
-            PCF.logger.debug("Injecting ModIdArgument serializer into ArgumentTypes");
-            ArgumentTypes.register(
-                    FORGE_MODID.toString(),
-                    ModIdArgument.class,
-                    new EmptyArgumentSerializer<>(ModIdArgument::new));
-            FORGE_MODID_ARG_REGISTERED = true;
-        }
-    }
+    private RegisterForgeArguments() {}
 
-    private static void registerForgeEnumArgument() {
-        if (!FORGE_ENUM_ARG_REGISTERED) {
-            PCF.logger.debug("Injecting EnumArgument serializer into ArgumentTypes");
-            ArgumentTypes.register(
-                    FORGE_ENUM.toString(),
-                    EnumArgument.class,
-                    new EmptyArgumentSerializer<>(new DummyEnumArgumentSupplier()));
-            FORGE_ENUM_ARG_REGISTERED = true;
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public static void applyFixes(ArgumentType<?> argumentType) {
+        if (IS_FORGE_14_15) {
+            switch (argumentType) {
+                case EnumArgument ignored -> {
+                    if (!FORGE_MODID_ARG_REGISTERED
+                            && ArgumentTypesUtil.getByClassMap().containsKey(ModIdArgument.class)) {
+                        PCF.logger.debug("Injecting ModIdArgument serializer into ArgumentTypes");
+                        ArgumentTypes.register(
+                                FORGE_MODID.toString(),
+                                ModIdArgument.class,
+                                new EmptyArgumentSerializer<>(ModIdArgument::new));
+                        FORGE_MODID_ARG_REGISTERED = true;
+                    }
+                }
+                case ModIdArgument ignored -> {
+                    if (!FORGE_ENUM_ARG_REGISTERED
+                            && ArgumentTypesUtil.getByClassMap().containsKey(EnumArgument.class)) {
+                        PCF.logger.debug("Injecting EnumArgument serializer into ArgumentTypes");
+                        ArgumentTypes.register(
+                                FORGE_ENUM.toString(),
+                                EnumArgument.class,
+                                new EmptyArgumentSerializer<>(
+                                        () -> EnumArgument.enumArgument(Enum.class)));
+                        FORGE_ENUM_ARG_REGISTERED = true;
+                    }
+                }
+                case null, default -> {}
+            }
         }
     }
 }
