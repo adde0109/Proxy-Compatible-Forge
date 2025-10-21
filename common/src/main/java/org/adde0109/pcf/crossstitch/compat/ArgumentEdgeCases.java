5b76cba3688a9ed0fe89774ce14b736b5d995b25
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-10-21 02:43:15 -0600

    Added an argument edge case for Bookshelf on versions 1.14.4-1.16.5 for its custom args under the vanilla namespace

--- common/src/main/java/org/adde0109/pcf/crossstitch/compat/ArgumentEdgeCases.java
+++ common/src/main/java/org/adde0109/pcf/crossstitch/compat/ArgumentEdgeCases.java
@@ -1,12 +1,13 @@
 package org.adde0109.pcf.crossstitch.compat;
 
 import dev.neuralnexus.taterapi.meta.MetaAPI;
+import dev.neuralnexus.taterapi.meta.MinecraftVersions;
 
 public final class ArgumentEdgeCases {
     private ArgumentEdgeCases() {}
 
     public static boolean isArgumentEdgeCase(String identifier) {
-        return isLivingThingsEdgeCase(identifier);
+        return isLivingThingsEdgeCase(identifier) || isBookshelfEdgeCase(identifier);
     }
 
     private static final boolean IS_LIVINGTHINGS_LOADED =
@@ -21,5 +22,23 @@ public final class ArgumentEdgeCases {
      */
     private static boolean isLivingThingsEdgeCase(String identifier) {
         return IS_LIVINGTHINGS_LOADED && identifier.equals("minecraft:entity");
+    }
+
+    private static final boolean IS_BOOKSHELF_LOADED =
+            MetaAPI.instance().isModLoaded("bookshelf")
+                    && MetaAPI.instance()
+                            .version()
+                            .isInRange(MinecraftVersions.V14_4, MinecraftVersions.V16_5);
+
+    /**
+     * Bookshelf adds "minecraft:hand" and "minecraft:loot" under the vanilla namespace
+     *
+     * @param identifier the argument type resource location
+     * @return true if the identifier is "minecraft:hand" or "minecraft:loot" and Bookshelf is
+     *     loaded
+     */
+    public static boolean isBookshelfEdgeCase(String identifier) {
+        return IS_BOOKSHELF_LOADED
+                && (identifier.equals("minecraft:hand") || identifier.equals("minecraft:loot"));
     }
 }
