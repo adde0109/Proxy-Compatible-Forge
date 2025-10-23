448714b21ca8fa8e3daf1b60ef9b6925b0cf83de
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-10-23 01:27:12 -0600

    Updated Bookshelf edge-case

--- common/src/main/java/org/adde0109/pcf/crossstitch/compat/ArgumentEdgeCases.java
+++ common/src/main/java/org/adde0109/pcf/crossstitch/compat/ArgumentEdgeCases.java
@@ -3,6 +3,8 @@ package org.adde0109.pcf.crossstitch.compat;
 import dev.neuralnexus.taterapi.meta.MetaAPI;
 import dev.neuralnexus.taterapi.meta.MinecraftVersions;
 
+import java.util.List;
+
 public final class ArgumentEdgeCases {
     private ArgumentEdgeCases() {}
 
@@ -14,17 +16,22 @@ public final class ArgumentEdgeCases {
             MetaAPI.instance().isModLoaded("bookshelf")
                     && MetaAPI.instance()
                             .version()
-                            .isInRange(MinecraftVersions.V14_4, MinecraftVersions.V16_5);
+                            .isInRange(MinecraftVersions.V14, MinecraftVersions.V17_1);
+
+    private static final List<String> BOOKSHELF_EDGE_CASES =
+            List.of("minecraft:enum", "minecraft:hand", "minecraft:loot", "minecraft:mod");
 
     /**
-     * Bookshelf adds "minecraft:hand" and "minecraft:loot" under the vanilla namespace
+     * Bookshelf registers multiple argument ids under the vanilla namespace <br>
+     * "minecraft:mod" -> 1.14.x <br>
+     * "minecraft:enum" -> 1.14 - 1.15.2, 1.17.1 <br>
+     * "minecraft:hand" -> 1.16.x - 1.16.5 <br>
+     * "minecraft:loot" -> 1.16.x - 1.17.1
      *
      * @param identifier the argument type resource location
-     * @return true if the identifier is "minecraft:hand" or "minecraft:loot" and Bookshelf is
-     *     loaded
+     * @return true if the identifier matches and Bookshelf is loaded
      */
-    public static boolean isBookshelfEdgeCase(String identifier) {
-        return IS_BOOKSHELF_LOADED
-                && (identifier.equals("minecraft:hand") || identifier.equals("minecraft:loot"));
+    private static boolean isBookshelfEdgeCase(String identifier) {
+        return IS_BOOKSHELF_LOADED && BOOKSHELF_EDGE_CASES.contains(identifier);
     }
 }
